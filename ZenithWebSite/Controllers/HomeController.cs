62a978c5c6e35a287b45681c466e6adcd2e8c6b9
Naman Sharma <namansharma@live.ca>
2017-10-15 16:38:23 -0700

    partially done home controller

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -11,13 +11,34 @@ namespace ZenithWebSite.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
 
         public ActionResult Index()
         {
-            var events = db.Events.Include(e => e.ActivityCategory);
-            return View(events.ToList());
+            ViewBag.Message = "This Week's Events at Zenith Society";
+            ApplicationDbContext db = new ApplicationDbContext();
 
+            int today = (int)DateTime.Now.DayOfWeek;
+            int difference;
+            if (today == 0) { difference = 6; }
+            else difference = today - 1;
+
+            DateTime monday = DateTime.Now.AddDays(-difference);
+            DateTime sunday = monday.AddDays(6);
+
+
+            /* List<Event> model = db.Events
+                 .Where(x => x.EventToDateTime.CompareTo(monday) >=0 )
+                 .Where(x => x.EventFromDateTime.CompareTo(sunday) <= 0)
+                 .Include(x => x.ActivityCategory).ToList();           
+                 */
+
+            List<Event> model = db.Events
+                .Where(x => x.EventToDateTime.CompareTo(DateTime.Now) >= 0)
+                .Where(x => x.EventFromDateTime.CompareTo(DateTime.Now) <= 6)
+                .Include(x => x.ActivityCategory).ToList();
+
+            return View(model);
+  
         }
     }
 }
