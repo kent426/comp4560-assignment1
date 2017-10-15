99a0cf74cc3b3aed3dfb0efab1b1a1e69146b374
Kent Huang <kenttttt0@gmail.com>
2017-10-15 02:37:13 -0700

    added home table style

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZenithDataLib.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
 
 namespace ZenithWebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var events = db.Events.Include(e => e.ActivityCategory);
+            return View(events.ToList());
+
         }
     }
 }
