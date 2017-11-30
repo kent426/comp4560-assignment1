e0614d7b581e327e16e858a0a32375be3a97e61b
Kent Huang <kenttttt0@gmail.com>
2017-11-29 18:52:55 -0800

    added a

---
+++ Models/DbInitializer.cs
@@ -0,0 +1,52 @@
+﻿using ass2.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ass2.Models
+{
+    public class DbInitializer
+    {
+
+        public static async Task Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+       RoleManager<IdentityRole> roleManager, ILogger<DbInitializer> logger)
+        {
+
+
+            //If there is already an Administrator role, abort
+            if (context.Roles.Any(r => r.Name == "Admin")) return;
+
+            //Create the Administartor Role
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
+
+            //Create the default Admin account and apply the Administrator role
+            string user = "a@a.a";
+            string password = "P@$$w0rd";
+            string username = "a";
+            await userManager.CreateAsync(new ApplicationUser { UserName = username , Email = user, EmailConfirmed = true }, password);
+            await userManager.AddToRoleAsync(await userManager.FindByNameAsync(user), "Admin");
+
+
+            //If there is already an Administrator role, abort
+            if (context.Roles.Any(r => r.Name == "Member")) return;
+
+            //Create the Administartor Role
+            await roleManager.CreateAsync(new IdentityRole("Member"));
+
+            //Create the default Admin account and apply the Administrator role
+            string muser = "m@m.m";
+            string mpassword = "P@$$w0rd";
+            string musername = "m";
+            await userManager.CreateAsync(new ApplicationUser { UserName = musername, Email = muser, EmailConfirmed = true }, mpassword);
+            await userManager.AddToRoleAsync(await userManager.FindByNameAsync(muser), "Member");
+
+
+
+            context.SaveChanges();
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ass2.Data;
+using Microsoft.AspNetCore.Identity;
+using ass2.Models;
 
 namespace ass2
 {
@@ -14,7 +18,28 @@ namespace ass2
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    var dbInitializerLogger = services.GetRequiredService<ILogger<DbInitializer>>();
+                    DbInitializer.Initialize(context, userManager, roleManager, dbInitializerLogger);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
