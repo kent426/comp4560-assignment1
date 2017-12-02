053f6486b995ad6db57e711410a607e21d6db2b5
Kent Huang <kenttttt0@gmail.com>
2017-12-02 10:35:22 -0800

    added api controllers

---
+++ Controllers/ActivityCategoriesApiController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ass2.Data;
+using ass2.Models;
+
+namespace ass2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ActivityCategories")]
+    public class ActivityCategoriesApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActivityCategoriesApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ActivityCategories
+        [HttpGet]
+        public IEnumerable<ActivityCategory> GetActivityCategories()
+        {
+            return _context.ActivityCategories;
+        }
+
+        // GET: api/ActivityCategories/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetActivityCategory([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var activityCategory = await _context.ActivityCategories.SingleOrDefaultAsync(m => m.ActivityCategoryId == id);
+
+            if (activityCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(activityCategory);
+        }
+
+        private bool ActivityCategoryExists(int id)
+        {
+            return _context.ActivityCategories.Any(e => e.ActivityCategoryId == id);
+        }
+    }
+}
---
+++ Controllers/EventsApiController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ass2.Data;
+using ass2.Models;
+
+namespace ass2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Events")]
+    public class EventsApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EventsApi
+        [HttpGet]
+        public IEnumerable<Event> GetEvents()
+        {
+            return _context.Events;
+        }
+
+        // GET: api/EventsApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEvent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @event = await _context.Events.SingleOrDefaultAsync(m => m.EventId == id);
+
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@event);
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.EventId == id);
+        }
+    }
+}
