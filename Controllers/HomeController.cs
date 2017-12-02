dc25c473d296b41a4cdf27638609aafbc63de9e7
Kent Huang <kenttttt0@gmail.com>
2017-12-02 00:09:38 -0800

    finished task 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,33 +5,56 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ass2.Models;
+using Microsoft.EntityFrameworkCore;
+using ass2.Data;
 
 namespace ass2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+                var today = DateTime.Now.Date;
+                DateTime monday;
+                DateTime sunday;
+                if (today.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    monday = today.AddDays(-6);
+                    sunday = today;
+                }
+                else
+                {
+                    monday = today.AddDays(-(int)today.DayOfWeek).AddDays(1);
+                    sunday = today.AddDays(-(int)today.DayOfWeek).AddDays(7);
+                }
+
+
+                //var events = db.Events.Include(e => e.ActivityCategory)
+                //.Where(e => (e.EventFromDateTime.Date >= monday && e.EventFromDateTime.Date <= sunday));
+
+                Dictionary<DateTime, List<Event>> eventsForAWeek = new Dictionary<DateTime, List<Event>>();
+
+                for (int x = 0; x < 7; x++)
+                {
+                    var thisday = monday.AddDays(x).Date;
+                    var eventsInDay = _context.Events.Include(e => e.ActivityCategory)
+                                        .Where(e => e.EventFromDateTime.ToString("MM/dd/yyyy") == thisday.ToString("MM/dd/yyyy") && e.IsActive == true).ToList();
+                    eventsInDay.Sort((i, j) => DateTime.Compare(i.EventFromDateTime, j.EventFromDateTime));
+                    eventsForAWeek.Add(thisday, eventsInDay);
+                }
+
+
+
+
+                return View(eventsForAWeek);
+
+            }
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+    
 }
